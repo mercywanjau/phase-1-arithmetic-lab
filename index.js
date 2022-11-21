120fa8df9b953fbbb63c452b38da95f6b47f32ea
MercyWanjau <mwanjau997@gmail.com>
2022-11-21 23:28:26 +0300

    phase-1-arithmetic-lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = 2
+const num2 = 31
+const num3 =16
+const num4 = 6
+const multiply = num1 * num2
+const max = Math.max(5, 10, 15, 20)
+const mod = 16 % 6
+const random = Math.floor(Math.random() * 1000 ) + 1
